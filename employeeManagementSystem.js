e4b530559d7c74a7065c6dbb2e6b660089f0164c
Skyler Tosh <skylerjtosh@gmail.com>
2020-03-07 13:42:10 -0800

    Added the initial inquirer prompts

--- employeeManagementSystem.js
+++ employeeManagementSystem.js
@@ -11,13 +11,68 @@ var connection = mysql.createConnection({
 	user: "root",
   
 	// Your password
-	password: "",
-	database: "seed"
+	password: "101ruBBish10$",
+	database: "ems"
   });
   
   connection.connect(function(err) {
 	if (err) throw err;
-	console.log("Connected to DB");
+	runSearch();
   });
+
+  function runSearch() {
+	  inquirer
+	  	.prompt({
+			  name: "action",
+			  type: "rawlist",
+			  message: "What would you like to do?",
+			  choices: [
+				  "View all Employees",
+				  "View all Employees by Department",
+				  "View all Employees by Manager",
+				  "Add Employee",
+				  "Remove Employee",
+				  "Update Employee Role",
+				  "Update Employee Manager",
+				  "View all Roles",
+				  "Add Role"
+			  ]
+		  })
+		  .then(function(answer) {
+			switch (answer.action) {
+				case "View all Employees":
+				break;
+
+				case "View all Employees by Department":
+				break;
+
+				case "View all Employees by Manager":
+				break;
+
+				case "Add Employee":
+				break;
+
+				case "Remove Employee":
+				break;
+
+				case "Update Employee Role":
+				break;
+
+				case "Update Employee Manager":
+				break;
+
+				case "View all Roles":
+				break;
+
+				case "Add Role":
+				break;
+
+				case "Remove Role":
+				break;
+			}
+		  });
+  }
+
+
   
   
