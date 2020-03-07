c07c7e163a30e6731bdeea1ea658b604cc834622
Skyler Tosh <skylerjtosh@gmail.com>
2020-03-07 12:17:02 -0800

    Preparing to code program

---
+++ employeeManagementSystem.js
@@ -0,0 +1,23 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+	host: "localhost",
+  
+	// Your port; if not 3306
+	port: 3306,
+  
+	// Your username
+	user: "root",
+  
+	// Your password
+	password: "",
+	database: "seed.sql"
+  });
+  
+  connection.connect(function(err) {
+	if (err) throw err;
+	console.log("Connected to DB");
+  });
+  
+  
